d4feba5c0ff7d99811c851e1b6716e6bfa22e4f1
camilleldn <camille@camillebaldock.co.uk>
2016-01-05 01:25:13 +0000

    Docker compose

--- web/prototype/settings/base.py
+++ web/prototype/settings/base.py
@@ -29,12 +29,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django_postgrespool',
-        'NAME': 'prototype',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': os.environ['DB_SERVICE'],
+        'PORT': os.environ['DB_PORT']
     }
 }
 
